de2ec0d5b44f9e51dcbb3c896f8aa69efb52e48d
AuthenTiiq <47777491+AuthenTiiq@users.noreply.github.com>
2024-12-28 12:59:27 +0100

    Update fabric.ts

--- src/Minecraft-Loader/loader/fabric/fabric.ts
+++ src/Minecraft-Loader/loader/fabric/fabric.ts
@@ -6,9 +6,9 @@
 import { getPathLibraries } from '../../../utils/Index.js';
 import download from '../../../utils/Downloader.js';
 
-import nodeFetch from 'node-fetch';
-import fs from 'fs';
-import path from 'path';
+import nodeFetch from 'node-fetch'
+import fs from 'fs'
+import path from 'path'
 import { EventEmitter } from 'events';
 
 export default class FabricMC extends EventEmitter {
@@ -20,24 +20,24 @@ export default class FabricMC extends EventEmitter {
     }
 
     async downloadJson(Loader) {
-        let build;
-        let metaData = await nodeFetch(Loader.metaData).then((res) => res.json());
+        let build
+        let metaData = await nodeFetch(Loader.metaData).then(res => res.json());
 
-        let version = metaData.game.find((version) => version.version === this.options.loader.version);
-        let AvailableBuilds = metaData.loader.map((build) => build.version);
+        let version = metaData.game.find(version => version.version === this.options.loader.version);
+        let AvailableBuilds = metaData.loader.map(build => build.version);
         if (!version) return { error: `FabricMC doesn't support Minecraft ${this.options.loader.version}` };
 
         if (this.options.loader.build === 'latest' || this.options.loader.build === 'recommended') {
             build = metaData.loader[0];
         } else {
-            build = metaData.loader.find((loader) => loader.version === this.options.loader.build);
+            build = metaData.loader.find(loader => loader.version === this.options.loader.build);
         }
 
         if (!build) return { error: `Fabric Loader ${this.options.loader.build} not found, Available builds: ${AvailableBuilds.join(', ')}` };
 
         let url = Loader.json.replace('${build}', build.version).replace('${version}', this.options.loader.version);
-        let json = await nodeFetch(url).then((res) => res.json()).catch((err) => err);
-        return json;
+        let json = await nodeFetch(url).then(res => res.json()).catch(err => err);
+        return json
     }
 
     async downloadLibraries(json) {
@@ -48,26 +48,18 @@ export default class FabricMC extends EventEmitter {
         let size = 0;
 
         for (let lib of libraries) {
-            // Ignorer la bibliothèque ASM 9.6
-            if (lib.name === 'org.ow2.asm:asm:9.6') {
-                console.log("Bibliothèque ASM 9.6 ignorée :", lib.name);
-                this.emit('check', check++, libraries.length, 'libraries');
-                continue;
-            }
-
             if (lib.rules) {
                 this.emit('check', check++, libraries.length, 'libraries');
                 continue;
             }
-
-            let file = {};
+            let file = {}
             let libInfo = getPathLibraries(lib.name);
             let pathLib = path.resolve(this.options.path, 'libraries', libInfo.path);
             let pathLibFile = path.resolve(pathLib, libInfo.name);
 
             if (!fs.existsSync(pathLibFile)) {
-                let url = `${lib.url}${libInfo.path}/${libInfo.name}`;
-                let sizeFile = 0;
+                let url = `${lib.url}${libInfo.path}/${libInfo.name}`
+                let sizeFile = 0
 
                 let res: any = await downloader.checkURL(url);
                 if (res.status === 200) {
@@ -80,8 +72,8 @@ export default class FabricMC extends EventEmitter {
                     folder: pathLib,
                     path: `${pathLib}/${libInfo.name}`,
                     name: libInfo.name,
-                    size: sizeFile,
-                };
+                    size: sizeFile
+                }
                 files.push(file);
             }
             this.emit('check', check++, libraries.length, 'libraries');
@@ -94,6 +86,6 @@ export default class FabricMC extends EventEmitter {
 
             await downloader.downloadFileMultiple(files, size, this.options.downloadFileMultiple);
         }
-        return libraries;
+        return libraries
     }
 }
